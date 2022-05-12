0a1fce4aa4fb419278b5510eedfe66aee1c43f8d
ewertoncoutinho <ewertonmacielcoutinho@gmail.com>
2022-05-12 14:11:35 -0300

    add prettier

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const serveStatic = require("serve-static");
+const express = require('express')
+const serveStatic = require('serve-static')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.use(serveStatic(".", { index: "index.html" }));
+app.use(serveStatic('.', { index: 'index.html' }))
 app.listen(port, () => {
-  console.log(
-    `started server on 0.0.0.0:${port}, url: http://localhost:${port}`
-  );
-});
+	console.log(
+		`started server on 0.0.0.0:${port}, url: http://localhost:${port}`
+	)
+})
--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
 function resizeListener() {
-	const height = window.innerHeight.toString();
-	const width = window.innerWidth.toString();
-	const screenH = window.screen.height.toString();
-	const screenW = window.screen.width.toString();
-	const dpr = window.devicePixelRatio.toFixed(2);
+	const height = window.innerHeight.toString()
+	const width = window.innerWidth.toString()
+	const screenH = window.screen.height.toString()
+	const screenW = window.screen.width.toString()
+	const dpr = window.devicePixelRatio.toFixed(2)
 
-	document.querySelector("#height").textContent = height;
-	document.querySelector("#width").textContent = width;
-	document.querySelector("#screenH").textContent = screenH;
-	document.querySelector("#screenW").textContent = screenW;
-	document.querySelector("#dpr").textContent = dpr;
+	document.querySelector('#height').textContent = height
+	document.querySelector('#width').textContent = width
+	document.querySelector('#screenH').textContent = screenH
+	document.querySelector('#screenW').textContent = screenW
+	document.querySelector('#dpr').textContent = dpr
 }
 
-window.addEventListener("resize", resizeListener);
-
-(function () {
-	resizeListener();
-})();
+window.addEventListener('resize', resizeListener)
+;(function () {
+	resizeListener()
+})()
