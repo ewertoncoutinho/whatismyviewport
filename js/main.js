d8a45f81fae2f5332378b29c0030b70784df06ac
ewertoncoutinho <ewertonmacielcoutinho@gmail.com>
2022-05-12 13:22:57 -0300

    add app

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function resizeListener() {
+	const height = window.innerHeight.toString();
+	const width = window.innerWidth.toString();
+	const screenH = window.screen.height.toString();
+	const screenW = window.screen.width.toString();
+	const dpr = window.devicePixelRatio.toFixed(2);
+
+	document.querySelector("#height").textContent = height;
+	document.querySelector("#width").textContent = width;
+	document.querySelector("#screenH").textContent = screenH;
+	document.querySelector("#screenW").textContent = screenW;
+	document.querySelector("#dpr").textContent = dpr;
+}
+
+window.addEventListener("resize", resizeListener);
+
+(function () {
+	resizeListener();
+})();
